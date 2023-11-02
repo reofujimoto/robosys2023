3966a233e875d32245f9637339382ef22dc7429c
Reo <reobsk.40@gmail.com>
2023-11-02 11:41:13 +0900

    Add test flow

--- test.bash
+++ test.bash
@@ -1,6 +1,19 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Reo Fujimoto
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ]
+
 
 
 
