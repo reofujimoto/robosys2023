63711477d815f5115729937a97e6d266b2110c44
Reo <reobsk.40@gmail.com>
2023-11-28 16:10:40 +0900

    add a command

--- test.bash
+++ test.bash
@@ -1,9 +1,9 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2023 Reo Fujimoto
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line $1
+	echo NG at Line ${1}
 	res=1
 }
 
@@ -11,9 +11,34 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-
 [ "${out}" = 15 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+### conversion_TEST ###
+ 
+out=$(./conversion.py 180)
+[ "${out}" = 換算: 3.141592653589793
+             radians関数: 3.141592653589793 
+] || ng ${LINENO}
+
+
+out=$(echo あ | ./conversion.py )
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./conversion.py )
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+ [ "$res" = 0 ] && echo OK
 exit $res
+
 
