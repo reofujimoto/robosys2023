64bccacb99c62ca59740a0720862f8169c850c05
Reo <reobsk.40@gmail.com>
2023-11-29 18:33:50 +0900

    add a command

--- test.bash
+++ test.bash
@@ -3,15 +3,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line ${1}
+	echo NG at Line $1
 	res=1
 }
+
 res=0
 
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = "15" ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
@@ -26,6 +27,7 @@ out=$(echo | ./plus)
 out=$(./conversion.py 180)
 [ "${out}" = "換算結果 3.141592653589793
 radians関数 3.141592653589793" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
